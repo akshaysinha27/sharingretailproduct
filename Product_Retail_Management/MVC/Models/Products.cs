b89ac6ab1cdee73e333f6c3e84d0ce11a0f7a339
Kumar Akshay <akshaysinha2712@outlook.com>
2021-02-02 17:39:57 +0530

    Final

---
+++ Product_Retail_Management/MVC/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC.Models
+{
+    public partial class Products
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public byte[] ImageName { get; set; }
+        public double? Rating { get; set; }
+    }
+}
